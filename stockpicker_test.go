f05ff1434efc5c3eecd8de42ff20a171d1bb53d0
simon.mittag <simonmittag@gmail.com>
2023-08-09 12:26:33 +1000

    doco

--- stockpicker_test.go
+++ stockpicker_test.go
@@ -10,11 +10,11 @@ func TestStockPicker(t *testing.T) {
 		Sample []int
 		Output int
 	}{
-		{"5", []int{10, 12, 4, 5, 9}, 5},
-		{"8", []int{14, 20, 4, 12, 5, 11}, 8},
-		{"16", []int{44, 30, 24, 32, 35, 30, 40, 38, 15}, 16},
-		{"-1", []int{10, 9, 8, 2}, -1},        //loss
-		{"0", []int{101, 79, 38, 24, 24}, -1}, //no profit
+		{"profit 5", []int{10, 12, 4, 5, 9}, 5},
+		{"profit 8", []int{14, 20, 4, 12, 5, 11}, 8},
+		{"profit 16", []int{44, 30, 24, 32, 35, 30, 40, 38, 15}, 16},
+		{"loss -1", []int{10, 9, 8, 2}, -1},               //loss
+		{"loss same val", []int{101, 79, 38, 24, 24}, -1}, //no profit
 	}
 
 	for _, tt := range tests {
